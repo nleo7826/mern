12db7bbea86f1f223bbfe4d684991f08ad24b7bb
Jsleo <jsleo906@gmail.com>
2023-01-15 18:24:08 +0800

    herosection -> homesection

---
+++ client/src/components/HomeSection.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HomeSection.css';
+
+function HeroSection() {
+  return (
+    <div className='home-container'>
+      <img src={'images/kathakali-home.jpg'} alt="kathakali-home" 
+        width="100%"
+        height="auto"
+      />
+      
+      <div className='home-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          // onClick={console.log('hey')}
+        >
+          DISCOVER KATHAKALI <i className='far fa-play-circle' />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+/* <iframe 
+title="YouTube video player"
+width="100%" 
+height="1215" 
+src="https://www.youtube.com/embed/GBbcYtkqVKQ?autoplay=1" 
+frameborder="0" 
+allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+      gyroscope; picture-in-picture; web-share" 
+allowfullscreen
+></iframe> */
+export default HeroSection;
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../App.css';
 import Cards from '../Cards';
-import HeroSection from '../HeroSection';
+import HeroSection from '../HomeSection';
 import Footer from '../Footer';
 
 function Home() {
